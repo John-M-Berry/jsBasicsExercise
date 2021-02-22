5a3924a3638d83d8f3745bbc84e7c10114201f99
MjrKong42 <mjr.kong42@gmail.com>
2021-02-22 12:49:37 -0600

    JavaScript Exercise w/ Bonus

---
+++ jsBasicsExercise.js
@@ -0,0 +1,51 @@
+// ONE
+console.log(42);
+
+// TWO
+// PRIMITIVES (Data Types are Numbers, Strings, Booleans, Null & Undefined)
+
+// THREE
+// OPERATORS (Addition, Subtraction, Multipication, Division, Exponentiation, Modulus)
+
+// FOUR
+console.log(5%2);
+console.log(5%4);
+
+// FIVE
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// SIX
+console.log("Wanda"+"Vision");
+
+// SEVEN
+console.log("John".length+"Berry".length);
+
+// EIGHT
+console.log("Avengers: Engame"[15]);
+
+// BONUS ONE
+console.log("\\");
+
+// BONUS TWO
+console.log(5**4-9);
+
+// BONUS THREE
+console.log("I".length-"Am".length+"Iron".length-"Man".length);
+console.log("I".length+"Am".length-("Iron".length%2)-"Man".length);
+
+// BONUS FOUR
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[6]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// BONUS FIVE
+var m=10
+var c=5
+var u=m+c
+
+console.log(u);
