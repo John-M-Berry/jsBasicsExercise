c27bc7852a4ddcf0048c57b3126abcb12060eca6
MjrKong42 <mjr.kong42@gmail.com>
2021-02-22 13:43:08 -0600

    JavaScript Exercise w/ Bonus

--- jsBasicsExercise.js
+++ jsBasicsExercise.js
@@ -21,7 +21,7 @@ console.log("Wanda"+"Vision");
 console.log("John".length+"Berry".length);
 
 // EIGHT
-console.log("Avengers: Engame"[15]);
+console.log("Avengers: Endgame"[16]);
 
 // BONUS ONE
 console.log("\\");
